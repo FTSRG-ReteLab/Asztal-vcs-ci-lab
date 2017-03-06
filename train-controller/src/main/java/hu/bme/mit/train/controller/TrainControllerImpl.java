58105b47805c0488fc327f8f6ac30f9718235747
helltp <arpifejes92@gmail.com>
2017-03-06 08:05:15 -0800

    bugfix

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -10,10 +10,13 @@ public class TrainControllerImpl implements TrainController {
 
 	@Override
 	public void followSpeed() {
-		if (referenceSpeed < 0) {
+		if (referenceSpeed < 0 ) {
 			referenceSpeed = 0;
 		} else {
 			referenceSpeed += step;
+			if(referenceSpeed < 0){
+				referenceSpeed =0;
+			}
 		}
 
 		enforceSpeedLimit();
